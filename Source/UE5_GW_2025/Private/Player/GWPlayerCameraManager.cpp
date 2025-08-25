ed30ff557589d65813b603ab22d68700a23d7979
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2025-08-25 15:31:27 +0900

    カメラを真下にできないようにした。移動アニメーションをブレンドスペースではなくアニメーションにしている。

---
+++ Source/UE5_GW_2025/Private/Player/GWPlayerCameraManager.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/GWPlayerCameraManager.h"
+
+AGWPlayerCameraManager::AGWPlayerCameraManager()
+{
+	// ピッチの最小最大を設定する
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
---
+++ Source/UE5_GW_2025/Public/Player/GWPlayerCameraManager.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "GWPlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GW_2025_API AGWPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+public:
+
+	// コンストラクタ
+	AGWPlayerCameraManager();
+
+};

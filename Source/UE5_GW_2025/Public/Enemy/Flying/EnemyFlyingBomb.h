b8b747017d7fd1926e1e8dcc97c372a66a1b2212
gao2968 <118335646+gao2968@users.noreply.github.com>
2025-10-27 13:11:21 +0900

    追加

---
+++ Source/UE5_GW_2025/Public/Enemy/Flying/EnemyFlyingBomb.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/Flying/EnemyFlying.h"
+#include "EnemyFlyingBomb.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_GW_2025_API AEnemyFlyingBomb : public AEnemyFlying
+{
+	GENERATED_BODY()
+	
+};
